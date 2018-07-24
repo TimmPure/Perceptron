ff640912aec3b77231ca08ea138862009597f8ec
TimmPure <T.j.m.voskuil@gmail.com>
2018-07-24 22:27:25 +0200

    Float[] inputs discontinued; general cleanup

--- Perceptron/Assets/Perceptron.cs
+++ Perceptron/Assets/Perceptron.cs
@@ -2,15 +2,14 @@
 
 public class Perceptron : MonoBehaviour
 {
-
-    public float[] inputs;
-    public float[] weights;
     const float bias = 1f;
     const float learningRate = 0.1f;
+    public float[] weights;
+    public int dataPoints = 100;
     public Coordinate[] dataSet;
-    public Coordinate dataPoint;
 
-    private int dataPoints = 100;
+
+    private Coordinate dataPoint;
     private float error = 0f;
 
     void Start()
@@ -23,6 +22,7 @@ public class Perceptron : MonoBehaviour
     private void CreateDataset()
     {
         dataSet = new Coordinate[dataPoints];
+
         for (int i = 0; i < dataSet.Length; i++)
         {
             dataSet[i] = new Coordinate();
@@ -54,7 +54,7 @@ public class Perceptron : MonoBehaviour
 
     private float Sum(Coordinate coord)
     {
-          return coord.x * weights[0] + coord.y * weights[1] + bias * weights[2];
+        return coord.x * weights[0] + coord.y * weights[1] + bias * weights[2];
     }
 
     private float Activate(Coordinate coord)
@@ -79,60 +79,14 @@ public class Perceptron : MonoBehaviour
         }
     }
 
-
-//    private void Process()
-//    {
-//        inputs = new float[inputCount];
-
-//        for (int i = 0; i < inputs.Length; i++)
-//        {
-//            Vector3 temp = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), 0f);
-//            temp.z = Label(temp);
-//            inputs[i] = temp;
-//            guess = Sum(i);
-//            Activate();
-//            Train(i);
-//        }
-//    }
-
-//private float Label(Vector3 toLabel)
-//{
-//    if (toLabel.x >= toLabel.y)
-//    {
-//        return -1f;
-//    } else
-//    {
-//        return 1f;
-//    }
-//}
-
-
-//private void Activate()
-//{
-//    if (guess >= 0f)
-//    {
-//        guess = 1f;
-//    } else
-//    {
-//        guess = -1f;
-//    }
-//    Debug.Log("Guess is " + guess);
-//}
-
-//private void Train(int i)
-//{
-//    error = inputs[i].z - guess;
-//    if (error != 0f)
-//    {
-//        weights.x += error * inputs[i].x * learningRate;
-//        weights.y += error * inputs[i].y * learningRate;
-//        weights.z += error * inputs[i].z * learningRate;
-//        Debug.Log("Weights are " + weights);
-//    }
-//}
-//// Update is called once per frame
-//void Update()
-//{
-
-//}
+    //private void Train(int i)
+    //{
+    //    error = inputs[i].z - guess;
+    //    if (error != 0f)
+    //    {
+    //        weights.x += error * inputs[i].x * learningRate;
+    //        weights.y += error * inputs[i].y * learningRate;
+    //        weights.z += error * inputs[i].z * learningRate;
+    //        Debug.Log("Weights are " + weights);
+    //    }
 }
